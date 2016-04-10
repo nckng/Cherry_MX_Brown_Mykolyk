f85503d844df847ddb64a791248ee25b2833f9ac
Nicholas Ng <Nicholas Ng>
2016-04-10 19:16:54 -0400

    finished act3

--- activity3/Magpie3.java
+++ activity3/Magpie3.java
@@ -45,6 +45,31 @@ public class Magpie3
 		{
 			response = "Tell me more about your family.";
 		}
+		else if (findKeyword(statement,"cat") >= 0 || findKeyword(statement,"dog") >= 0)		    {
+		    response = "Tell me moar about your pets!";
+		}
+
+			else if (findKeyword(statement,"brown") >= 0)
+		{
+			response = "He sounds like a good teacher.";
+		}
+		else if (statement.length() == 0)
+		{
+			response = "Say something please";
+		}
+		else if (findKeyword(statement,"no") >= 0)
+		{
+			response = "why not?";
+		}
+		else if (findKeyword(statement,"you") >= 0)
+		{
+			response = "me???";
+		}
+		else if (findKeyword(statement,"...") >= 0)
+		{
+		    response = "go on.";
+		}
+	       
 		else
 		{
 			response = getRandomResponse();
