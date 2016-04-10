45aa812f16d91e8c37f65ed6b1b7def0b3992f1d
max_anderson <max.anderson819@gmail.com>
2016-04-10 19:53:23 +0000

    Activity 2 finished.

--- activity2/Magpie2.java
+++ activity2/Magpie2.java
@@ -42,6 +42,30 @@ public class Magpie2
 		{
 			response = "Tell me more about your family.";
 		}
+		else if (statement.indexOf("cat") >= 0 || statement.indexOf("dog") >= 0)
+		{
+			response = "Tell me more about your pets!";
+		}
+		else if (statement.indexOf("brown") >= 0)
+		{
+			response = "He sounds like a good teacher.";
+		}
+		else if (statement.length() == 0)
+		{
+			response = "Say something please";
+		}
+		else if (statement.indexOf("no") >= 0)
+		{
+			response = "why not?";
+		}
+		else if (statement.indexOf("you") >= 0)
+		{
+			response = "me???";
+		}
+		else if (statement.indexOf("...") >= 0)
+		{
+			response = "go on.";
+		}
 		else
 		{
 			response = getRandomResponse();
@@ -55,7 +79,7 @@ public class Magpie2
 	 */
 	private String getRandomResponse()
 	{
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -75,6 +99,14 @@ public class Magpie2
 		else if (whichResponse == 3)
 		{
 			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			response = "Wow!";
+		}
+		else if (whichResponse == 5)
+		{
+			response = "Wait... What? Explain that again please.";
 		}
 
 		return response;
